b131c330389eeb5ac6d0d02b1f901e45f7af85eb
S-Farhan05 <159949925+S-Farhan05@users.noreply.github.com>
2024-09-14 02:03:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+let answer = await inquirer.prompt({
+    name: "Sentence",
+    type: "input",
+    message: "Enter a sentence to find how many words it contains :"
+});
+const wordCount = answer.Sentence.trim().split(" ");
+console.log(`Your sentence contain ${wordCount.length} words`);
